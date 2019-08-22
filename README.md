a7a40f10ccc1c4cfaf4cb26a4c52c59d090ea166
georgk10 <georgk98@gmail.com>
2019-08-22 16:03:12 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Binance Arbitrator
+
+This bot is meant to trade **triangular arbitrage** on **binance**. Using the binance websocket api to retrieve data, it's pretty fast and executes trades at the blink of an eye. Nevertheless I didn't managed this to be profitable, as it's still to slow to take advantage of triangular arbitrage opportunities. Maybe with a good server location and low latency contection this could be profitable.
+
+**Use at your own risk. I'm not responsible for any losses and/or  damage caused by this code**
+
+
+
+
+
+
+# How it works
+
+The script tracks the prices of each altcoin on the USDT **and**  the BTC market. If it detects an difference, greater than the cumulated trading fee's, it buys the altcoin on the cheaper market and sells ob the more expensive. Below the both possibilities are visualized:
+
+1. Buy on USDT Market, Sell on BTC Market
+```mermaid
+graph LR
+A((USDT)) --> B((Altcoin))
+B --> C((BTC))
+C --> A((USDT))
+```
+
+2. Buy on BTC Market, Sell on USDT Market
+```mermaid
+graph LR
+A((USDT)) --> B((BTC))
+B --> C((Altcoin))
+C --> A((USDT))
+```
+## Dependencies
+
+This Bot is written and tested in Python3.6. It depends on requests, websockets & python-binance modules. You can install these dependencies by naviagting to the project folder and typing: 
+>pip install -r requirements.txt 
+
+## Setup
+
+1.  Download this repo
+2. Create an Binance account
+3. Make sure to have at least 10 USDT in your binance account
+4. Create an api key and write down both (the public and secret) keys
+5. Put your api keys inside the config.json file
+6. (Optionally) adjust the maximum trade amount 
+7. (Optionally) adjust the trading currencies in config.json - Note that a currencies must be available to trade on **both** the USDT **and** BTC markets, in order to be traded.
+8. Start bnArb.py 
+
+
+##
+
+I would love to know if you could manage to make some profits out of this. For any questions, inquiries or suggestions, contact me at
+georgk98@gmail.com
---
+++ bnArb.py
@@ -0,0 +1,181 @@
+import websockets, asyncio, json, datetime, requests, time, uuid
+from decimal import Decimal
+from threading import Thread
+from binance.client import Client
+
+class BnArber:
+    """
+    Triangular Arbitrage Bot for Binance.
+    Trading Patterns:
+        - USDT -> BTC -> ALTCOIN -> USDT
+        - USDT -> ALTCOIN -> BTC -> USDT
+    """
+    def __init__(self, curs, public, secret, max_amount):
+        self.url = "wss://stream.binance.com:9443/stream?streams=btcusdt@depth5/"
+        self.curs = curs
+        self.data = {}
+        self.timeout = False
+        self.money = 0
+        self.min_amount = 5
+        self.max_amount = max_amount
+        self.client = Client(public, secret)
+        self.precision = {}
+        for i in self.client.get_exchange_info()['symbols']:
+            for f in i["filters"]:
+                if f["filterType"] == "LOT_SIZE":
+                    if float(f["minQty"]) <= 1:
+                        self.precision[i["symbol"]] = str(int(1/float(f["minQty"]))).count("0")
+                    else:
+                        self.precision[i["symbol"]] = -1*int(f["minQty"].count("0"))
+                        
+    async def run(self):
+        print("Arbitrator started...")
+        print("Operating Markets:", ', '.join(self.curs))
+        heartbeat = datetime.datetime.utcnow() - datetime.timedelta(hours=1)
+        for cur in self.curs:
+            self.url += cur.lower()+"usdt@depth5/"+cur.lower()+"btc@depth5/"
+        async with websockets.connect(self.url) as websocket:
+            async for message in websocket:
+                self.handle_data(message)
+                if not self.timeout:
+                    self.timeout = True
+                    Thread(target=self.get_rates, args=(websocket,)).start()
+                if heartbeat < datetime.datetime.utcnow() - datetime.timedelta(hours=1):
+                    print("Balance:", self.get_balance("USDT"), "USDT")
+                    heartbeat = datetime.datetime.utcnow()
+                    
+
+    def handle_data(self, message):
+        message = json.loads(message)
+        market_id = message["stream"].split("@")[0]
+        asks = {}
+        for ask in message["data"]["asks"]:
+            asks[Decimal(ask[0])] = Decimal(ask[1])
+        bids = {}
+        for bid in message["data"]["bids"]:
+            bids[Decimal(bid[0])] = Decimal(bid[1])
+        self.data[market_id.upper()] = {"ask":asks, "bid":bids}
+                    
+    def get_rates(self, websocket):
+        fee = Decimal(1-(0.999**3))
+        for cur in self.curs:
+            
+            try:
+                r = (1/self.get_ask(cur+"USDT")[0]*self.get_bid(cur+"BTC")[0]*self.get_bid("BTCUSDT")[0])-1
+                am1 = (self.get_ask(cur+"USDT")[1]*self.get_bid(cur+"USDT")[0])
+                am2 = (self.get_bid(cur+"BTC")[1]*self.get_bid(cur+"BTC")[0])*self.get_bid("BTCUSDT")[0]
+                am3 = (self.get_bid("BTCUSDT")[1]*self.get_bid("BTCUSDT")[0])
+                am4 = (self.get_bid(cur+"BTC")[1]*self.get_bid(cur+"USDT")[0])
+                euro_available = min(am1, am2, am3, am4)
+                if r > fee and euro_available > self.min_amount:
+                    euro_available = min([euro_available, self.max_amount])
+                    trade_amount = self.floor(euro_available/self.get_ask(cur+"USDT")[0], self.precision[cur+"USDT"])
+                    order_success = self.order(cur+"USDT", "BUY", trade_amount)
+                    if order_success:
+                        trade_amount = self.floor(trade_amount*0.999, self.precision[cur+"BTC"])
+                        order_success = self.order(cur+"BTC", "SELL", trade_amount)
+                        if not order_success:
+                            self.sell_all()
+                            time.sleep(10)
+                            print("Balance:", self.get_balance("USDT"), "USDT")
+                            continue
+                        trade_amount = self.floor((trade_amount*0.999)*float(self.get_bid(cur+"BTC")[0]), self.precision["BTCUSDT"])
+                        order_success = self.order("BTCUSDT", "SELL", trade_amount)
+                        if not order_success:
+                            self.sell_all()
+                            time.sleep(10)
+                            print("Balance:", self.get_balance("USDT"), "USDT")
+                            continue
+                        print("Balance:", self.get_balance("USDT"), "USDT")
+                    else:
+                        pass    
+                
+                r = (1/self.get_ask("BTCUSDT")[0]/self.get_ask(cur+"BTC")[0]*self.get_bid(cur+"USDT")[0])-1
+                am1 = (self.get_ask("BTCUSDT")[1]*self.get_bid("BTCUSDT")[0])
+                am2 = (self.get_ask(cur+"BTC")[1]*self.get_bid(cur+"BTC")[0])*self.get_bid(cur+"USDT")[0]
+                am3 = (self.get_bid(cur+"USDT")[1]*self.get_bid(cur+"USDT")[0])
+                am4 = (self.get_bid(cur+"BTC")[1]*self.get_bid(cur+"USDT")[0])
+                euro_available = min(am1, am2, am3, am4)
+                if r > fee and euro_available > self.min_amount:
+                    euro_available = min([euro_available, self.max_amount])
+                    trade_amount = self.floor(euro_available/self.get_ask("BTCUSDT")[0], self.precision["BTCUSDT"])
+                    order_success = self.order("BTCUSDT", "BUY", trade_amount)
+                    if order_success:
+                        trade_amount = self.floor(trade_amount*0.999, self.precision[cur+"BTC"])
+                        order_success = self.order(cur+"BTC", "BUY", trade_amount)
+                        if not order_success:
+                            self.sell_all()
+                            print("Balance:", self.get_balance("USDT"), "USDT")
+                            time.sleep(10)
+                            continue
+                        trade_amount = self.floor(trade_amount*0.999, self.precision[cur+"USDT"])
+                        order_success = self.order(cur+"USDT", "SELL", trade_amount)
+                        if not order_success:
+                            self.sell_all()
+                            print("Balance:", self.get_balance("USDT"), "USDT")
+                            time.sleep(10)
+                            continue
+                        print("Balance:", self.get_balance("USDT"), "USDT")
+                        print(self.old_data)
+                    else:
+                        pass
+            except KeyError:
+                pass
+        self.timeout = False
+
+    def get_balance(self, cur):
+        try:
+            re = self.client.get_asset_balance(asset=cur)
+            return re["free"] 
+        except:
+            return 0
+
+    def sell_all(self):
+        try:
+            for cur in self.curs + ["BTC"]:
+                time.sleep(5)
+                amount = self.floor(self.get_balance(cur), self.precision[cur+"USDT"])
+                if amount*float(self.get_bid(cur+"USDT")[0]) > self.min_amount:
+                    self.order(cur+"USDT", "SELL", amount)
+        except:
+            pass
+
+    def order(self, market, side, amount):
+        try:
+            print(market, side, amount)
+            if side.lower() == "buy":
+                re = self.client.create_order(symbol=market, side=Client.SIDE_BUY, type=Client.ORDER_TYPE_MARKET,quantity=str(amount))
+            elif side.lower() == "sell":
+                re = self.client.create_order(symbol=market, side=Client.SIDE_SELL, type=Client.ORDER_TYPE_MARKET,quantity=str(amount))
+            if re["status"] == "FILLED":
+                return True
+        except:
+            return False
+    
+    def get_bid(self, market):
+        price = max(self.data[market]["bid"].keys())
+        size = self.data[market]["bid"][price]
+        return (price, size)
+
+    def get_ask(self, market):
+        price = min(self.data[market]["ask"].keys())
+        size = self.data[market]["ask"][price]
+        return (price, size)
+
+    def floor(self, nbr, precision):
+        nbr = float(nbr)
+        if precision == 0:
+            return int(nbr)
+        else:
+            return int(nbr*10**precision)/10**precision
+        
+        
+        
+
+with open("config.json", "r") as file:
+    data = json.loads(file.read())
+ 
+bn = BnArber(data["currencies"], data["public"], data["secret"], data["max_amount"])
+asyncio.get_event_loop().run_until_complete(bn.run())
+
+
